419fe3fa74ac9ed3cc20c9aec754c21bd3737f12
Carrick <kartikgupta0007@gmail.com>
2025-07-16 16:38:04 +0530

    Initial Commit

---
+++ TheekKaro/TheekKaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheekKaroApp.swift
+//  TheekKaro
+//
+//  Created by Kartik Gupta on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TheekKaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheekKaroTests/TheekKaroTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheekKaroTests.swift
+//  TheekKaroTests
+//
+//  Created by Kartik Gupta on 16/07/25.
+//
+
+import Testing
+@testable import TheekKaro
+
+struct TheekKaroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
